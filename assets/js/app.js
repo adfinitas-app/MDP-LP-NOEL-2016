c5d284c9a4b99b6c40c9c9a61da478ab9cfac66d
bruere_s <sebastien.bruere@epitech.eu>
2016-11-03 11:46:23 +0100

    OK

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,23 @@
 $(document).ready(function() {
+	var x = ($(window).innerWidth() - $('#cta-video').innerWidth()) / 2;
+	$('#cta-video').css({"right" : x + 'px'});
+	x = ($(window).innerWidth() - $('#text-video').innerWidth()) / 2;
+	$('#text-video').css({"right" : x + 'px'});
+	$('#video-header').css({ width: $(window).innerWidth() + 'px', height: $(window).innerHeight() - $('#header').innerHeight() + 'px'});
+	$('#container-video-header').css({height: $(window).innerHeight() - $('#header').innerHeight() + 'px'})
 
+
+
+
+	var iframe1 = document.querySelector('#video-header');
+	var player1 = new Vimeo.Player(iframe1);
+	player1.on('play', function() {
+		$('#text-video').hide();
+	});
 });
 
 $(window).resize(function() {
-	
+
 });
 
 
